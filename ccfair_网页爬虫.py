cde24c2f5c4b0e8ce142ad701f2a46ae79a087cc
shuminghao <shuminghao677@gmail.com>
2026-01-28 00:01:22 +0800

    add:各种详细横向数据 两个图

--- ccfair_网页爬虫.py
+++ ccfair_网页爬虫.py
@@ -4,46 +4,52 @@ import pandas as pd
 import re
 import time
 from urllib.parse import urljoin
+from pyecharts.charts import Bar
+from pyecharts import options as opts
 
-base_url="https://www.cityu.edu.hk/csci/news-events/events/2026/stem-career-fair-2026?JobDepartment=%7BBEDAA9D8-7D6A-45F1-8BD4-3D378700E446%7D&JobType=%7B2D4FFA7E-EC2D-4F80-856D-35E770A06676%7D&JobEduLevel=%7BE6C6AD4D-D7AE-40ED-8DCD-4FCF214BDB27%7D&q="
 final_data=[]
 
-response=requests.get(base_url)
-soup=BeautifulSoup(response.text,"html.parser")
-print("开始抓取")
+for i in range(0,4):
+    base_url1="https://www.cityu.edu.hk/csci/news-events/events/2026/stem-career-fair-2026?page="
+    base_url=base_url1+str(i)
+    print(base_url)
 
-interns=soup.find_all("tr",class_="col-md-12 mb-3")
-for intern in interns:
-    company_name=intern.find("td",class_="views-field views-field-field-company-name").p.get_text()
-    job_title=intern.find("td",class_="views-field views-field-title").a.get_text()
-    relative_path=intern.find("td",class_="views-field views-field-title").a["href"]
-    detail_url=urljoin(base_url,relative_path)
-    print(detail_url)
+    response=requests.get(base_url)
+    soup=BeautifulSoup(response.text,"html.parser")
+    print(f"开始抓取第{i+1}页")
 
-    detail_response=requests.get(detail_url)
-    detail_soup=BeautifulSoup(detail_response.text,"html.parser")
-    addr_tag=detail_soup.find("div",class_="clearfix text-formatted field field--name-field-application-procedure field--type-text-long field--label-above")
-
-    if addr_tag:
-        all_text=addr_tag.get_text(separator=" ",strip=True)
-        urls=re.findall(r"https?://[^\s]+",all_text)#urls_lists
-        emails=re.findall(r"[\w.-]+@[\w.-]+",all_text)#emails_lists
-
-        total_addr=urls+emails
-
-        addr_str=",".join(total_addr)
-        if addr_str:
-            print("此时申请地址是:   {addr_str}")
-            application_addr=addr_str
-        else:
-            application_addr="暂未提供详细申请地址"
-
-    else:
-        application_addr="没有申请模块"
-  
+    interns=soup.find_all("tr",class_="col-md-12 mb-3")
+    for intern in interns:
+        try:
+            company_name=intern.find("td",class_="views-field views-field-field-company-name").p.get_text()
+            job_title=intern.find("td",class_="views-field views-field-title").a.get_text()
+            department_text=intern.find("td",class_="views-field views-field-field-department").get_text()
+            education_text=intern.find("td",class_="views-field views-field-field-education-level").get_text()
+            relative_path=intern.find("td",class_="views-field views-field-title").a["href"]
+            detail_url=urljoin(base_url,relative_path)
+            print(detail_url)
     
-    final_data.append({"Company Name":company_name,"job Title":job_title,"Application Addr":application_addr})
-    print(application_addr)
-    time.sleep(1)
+            detail_response=requests.get(detail_url)
+            detail_soup=BeautifulSoup(detail_response.text,"html.parser")
+            #抓取job description
+            job_description=detail_soup.find("div",class_="clearfix text-formatted field field--name-body field--type-text-with-summary field--label-above")
+            job_text=job_description.get_text(strip=True)if job_description else ""
+            job_text_clear=job_text.replace("Job Description","").strip()
+
+            application_procedure=detail_soup.find("div",class_="clearfix text-formatted field field--name-field-application-procedure field--type-text-long field--label-above")
+            app_text=application_procedure.get_text(strip=True) if application_procedure else ""
+            app_text_clear=app_text.replace("Application Procedure","").strip()
+
+            
+
+
+            final_data.append({"Company Name":company_name,"Job Title":job_title,"Job description":job_text_clear,"Application Procedure":app_text_clear,"Education Level": education_text,"Department":department_text})
+            print(f"目前抓取到公司是{company_name}")
+            time.sleep(0.5)
+        except Exception as e:
+            print(f"抓取{detail_url}时失败,失败原因是{e}")
+            continue
+
+
 df=pd.DataFrame(final_data)
-df.to_excel("cc_fair_cs专业实习招聘信息.xlsx",index=False)
+df.to_csv("stem_fair_2026_raw.csv",index=False,encoding="utf-8-sig")
